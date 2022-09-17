1adf72ffc0c8926f2b746b4cbd8722ac1636668a
Rebbi Xu <rebbi.xu123@gmail.com>
2022-09-17 13:31:58 -0400

    nice

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from email.mime import image
 import tkinter as tk
-import pyglet
+from PIL import ImageTk, Image
 
 window = tk.Tk()
 window.title('ASignL')
@@ -8,24 +9,36 @@ screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
 window_width = 960
-window_height = 540
+window_height = 700
 
 center_x = int(screen_width / 2 - window_width / 2)
 center_y = int(screen_height / 2 - window_height / 2)
 
 window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-greeting = tk.Label(text="AsignL", fg="#f58853", bg = "#e3cdcd", width=100, height=1, font=("Times 40"))
+greeting = tk.Label(text="AsignL", fg="#f58853",bg = "#e3cdcd", width=100, height=1, font=("Times 40"))
 greeting.pack()
 
 frame1 = tk.Frame(master=window, height=370, bg="#d3c7cd")
 frame1.pack(fill=tk.X)
 
-output = tk.Label(text= "Translation: ", height=170, bg="#e3cdcd", anchor='w')
+#video goes here
+frame2 = tk.Frame(master=window, height=400, bg="#FFFFFF")
+frame2.pack(fill=tk.X)
+
+output = tk.Label(text= "Translation: ", height=0, bg="#e3cdcd", anchor='w', font=("Times 30"))
 output.pack(fill=tk.X)
 
-frame4 = tk.Frame(master=window, height=999, bg="#d3c7cd")
-frame4.pack(fill=tk.X)
+#translation text goes here
+frame3 = tk.Frame(master=window, height=999, bg="#d3c7cd")
+frame3.pack(fill=tk.X)
+
+image1 = Image.open("delphianyearbooktemplate (1).png")
+image1 = image1.resize((60, 60), Image.ANTIALIAS)
+
+test = ImageTk.PhotoImage(image1)
+label1 = tk.Label(frame1, image=test)
+label1.pack()
 
 window.mainloop()
 
